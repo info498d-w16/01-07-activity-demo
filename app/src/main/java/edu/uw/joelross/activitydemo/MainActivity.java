16741c0d03c4517970909451fd236d56f79828c9
Joel Ross <joelross@uw.edu>
2016-01-07 12:27:13 -0800

    End of lecture code

---
+++ app/src/main/java/edu/uw/joelross/activitydemo/MainActivity.java
@@ -0,0 +1,69 @@
+package edu.uw.joelross.activitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Demo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+
+        Log.v(TAG, "onStart called");
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
+    public void launchSecondActivity(View v){
+        Log.i(TAG,"Button was pressed!");
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
